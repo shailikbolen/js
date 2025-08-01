12ab02c82810010ce225247ff7819807557f3c2d
shailikbolen <bshailik@gmail.com>
2025-08-01 06:35:18 +0000

    done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name="Shailik"
+let age=21
+let isloggedIn=false
+//null
+//undefined
+//boolean=true/false
+//int =2^53
+//bigint
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(isloggedIn)
