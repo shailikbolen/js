6f8c7f51d6634bee52b64bfb5bd8a739d71e7436
shailikbolen <bshailik@gmail.com>
2025-08-01 07:30:33 +0000

    operations & conversion done

--- .devcontainer/01_basics/03_datatypeconversion.js
+++ .devcontainer/01_basics/03_datatypeconversion.js
@@ -9,12 +9,47 @@
 //"33abc" => NaN(not in number)
 //true =1 ;false=>0
 
-// let isloggedIn=1
-// let booleanIsLoggedIn=Boolean (isloggedIn)
-// console.log(booleanIsLoggedIn)
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean (isloggedIn)
+console.log(booleanIsLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log( typeof stringNumber)
+
+// ***************** Operations ******************//
+let value =3
+let negvalue =-value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+str1 ="hello"
+str2=" shailik"
+str3 =str1+str2
+console.log(str3)
+
+
+console.log("1"+2) 
+//12
+console.log(1+"2") 
+//12
+console.log("1" +2+2)
+//  //122 1 is string so the rest 2 is also string
+console.log(1+2+"2") 
+//32 first is Number so all are converted to number automatically
+
+console.log(+true)
+//+ is Number operator so it changes boolean to number
+
+let GameCounter =100
+let y=GameCounter++
+console.log({GameCounter},{y})
+
 
