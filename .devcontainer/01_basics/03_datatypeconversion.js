bc1040f64ef0ebe25f2223cc7f901c91fb73e626
shailikbolen <bshailik@gmail.com>
2025-08-01 06:49:57 +0000

    cobersion of datatypes

---
+++ .devcontainer/01_basics/03_datatypeconversion.js
@@ -0,0 +1,20 @@
+// let score =null
+// console.log(typeof score)
+
+// let valurInNUmber=Number(score)
+// console.log(typeof valurInNUmber)
+// console.log(valurInNUmber)
+
+//"33" => 33
+//"33abc" => NaN(not in number)
+//true =1 ;false=>0
+
+// let isloggedIn=1
+// let booleanIsLoggedIn=Boolean (isloggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
+
