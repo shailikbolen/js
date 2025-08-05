2aa1c65012015ea816a24b180ad6e69f66c2304b
shailikbolen <bshailik@gmail.com>
2025-08-05 07:32:30 +0000

    change1

--- .devcontainer/01_basics/06_nums_and_maths.js
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -3,3 +3,10 @@ console.log(score)
 
 const balance =new Number(100)
 console.log(balance)
+
+const Decimal= 23.8988
+console.log(Decimal.toPrecision(3))
+
+const hundreds =100000
+console.log(hundreds.toLocaleString('en-IN'))
+//if we don't write en-IN it will give assign to International Number system
