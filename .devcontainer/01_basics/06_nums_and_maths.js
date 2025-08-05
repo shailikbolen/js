6c650e9df54a5402b0e1cbc41944ab7d1b9c2e7a
shailikbolen <bshailik@gmail.com>
2025-08-05 11:45:46 +0000

    done Maths

--- .devcontainer/01_basics/06_nums_and_maths.js
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -10,3 +10,22 @@ console.log(Decimal.toPrecision(3))
 const hundreds =100000
 console.log(hundreds.toLocaleString('en-IN'))
 //if we don't write en-IN it will give assign to International Number system
+
+
+//++++++++++++++++++++++  Maths ++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(-4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,3,6,8))
+// console.log(Math.max(4,3,6,8))
+
+console.log(Math.floor((Math.random()*10)) +1) // to return gretaer than 1
+
+// Math.random gives value between 0 and 1
+ const min =10
+ const max =20
+ // now to get in a range of 10(min) and 20(max)
+ console.log(Math.floor(Math.random() *(max-min+1))+min)
