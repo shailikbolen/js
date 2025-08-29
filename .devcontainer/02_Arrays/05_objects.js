f1db5c6d1e81fc36c85f7956a624bef98fdd92a9
shailikbolen <bshailik@gmail.com>
2025-08-29 05:45:06 +0000

    json talk

---
+++ .devcontainer/02_Arrays/05_objects.js
@@ -0,0 +1,16 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+//course.coursename
+const{courseInstructor: instructor}=course
+console.log(instructor);
+//json
+// {
+//     "name":"SHailik",
+//     "coursename":"js",
+//     "price":"free"
+// }
+
+// API are json format
