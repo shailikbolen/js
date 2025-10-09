cf68bd06ff1037004f989dbecc9c1ca49048bddb
shailikbolen <bshailik@gmail.com>
2025-10-09 06:45:23 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,21 @@
+const month="march"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+//break is used to break the control flow
