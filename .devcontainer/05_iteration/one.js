6903289bde13209e0574f571d52b8a1c34e654b9
shailikbolen <bshailik@gmail.com>
2025-10-10 06:21:59 +0000

    for-loop

---
+++ .devcontainer/05_iteration/one.js
@@ -0,0 +1,21 @@
+//for
+
+for (let i =0; i<=10;i++){
+    const element=i;
+    if (element==5) {
+        console.log("5 is best number");
+        
+    }
+    //console.log(element);
+    
+}
+
+for (let i = 0; i <=10; i++) {
+    console.log(`Outer loop: ${i}`);
+    for (let j = 0; j <=10; j++) {
+        //console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(i+ '*'+j +'=' +i*j);
+        
+    }
+    
+}
