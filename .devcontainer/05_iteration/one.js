168932f5649dd87046ad792648ba44648af2c144
shailikbolen <bshailik@gmail.com>
2025-10-11 10:29:50 +0000

    for-loop || break-continue

--- .devcontainer/05_iteration/one.js
+++ .devcontainer/05_iteration/one.js
@@ -6,16 +6,40 @@ for (let i =0; i<=10;i++){
         console.log("5 is best number");
         
     }
-    //console.log(element);
-    
+    //console.log(element); Error as it is printing element which is declared inside loop
+     
 }
 
 for (let i = 0; i <=10; i++) {
     console.log(`Outer loop: ${i}`);
     for (let j = 0; j <=10; j++) {
-        //console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(`Inner loop value ${j} and inner loop ${i}`);
         console.log(i+ '*'+j +'=' +i*j);
         
     }
     
+}
+
+let myArr=["flash","batman","superman"]
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+}
+
+//break and continue
+
+for (let index = 1; index <=20; index++) {
+    if (index==5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of i is ${index}`)    
+}
+
+for (let index = 1; index <=20; index++) {
+    if (index==5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`)    
 }
