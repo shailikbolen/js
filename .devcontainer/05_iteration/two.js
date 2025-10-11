b6debf31ce57969aceb36fd0f50164e1e7a332e3
shailikbolen <bshailik@gmail.com>
2025-10-11 10:38:49 +0000

    while || do-while

---
+++ .devcontainer/05_iteration/two.js
@@ -0,0 +1,19 @@
+let index =0
+while (index<=10) {
+    //console.log(`Value of index is ${index}`);
+    index +=2
+}
+
+let myArr=["iron-man","Captain","hulk"]
+let i=0
+while (i<myArr.length) {
+    //console.log(`Value is ${myArr[i]}`);
+    i+=1
+}
+
+let score=11
+// if 11 is written it will print 11 and then check the cond in while and then as cond fails it stops the loop
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10)
